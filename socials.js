0a4cf22246d754fba88f458923cefe6aa4ff5778
Navraj Kalsi <navrajkalsi@icloud.com>
2026-08-31 05:46:07 -0400

    blog done

---
+++ socials.js
@@ -0,0 +1,11 @@
+const socials = {
+  github: "https://www.github.com/navrajkalsi",
+  linkedin: "https://linkedin.com/in/navrajkalsi",
+  reddit: "https://reddit.com/u/navrajkalsi",
+  twitter: "https://twitter.com/navrajkalsi",
+  email: "mailto:navrajkalsi.com",
+};
+
+function visit_url(url) {
+  window.open(url, "_blank");
+}
